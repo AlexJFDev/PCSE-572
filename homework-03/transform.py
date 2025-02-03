47505cd7ca549c9754123da5d94decf0e2e41e53
alex <alexjfuhr@gmail.com>
2025-02-03 15:34:48 -0500

    Added multiplication to transform

--- homework-03/transform.py
+++ homework-03/transform.py
@@ -2,11 +2,19 @@ from rotation import Rotation
 import numpy as np
 
 class Transformation:
+    translation: np.ndarray[float, float]
+    rotation: Rotation
+    transformation_matrix: np.ndarray[float, float]
+
     def __init__(self, translation: tuple[float, float, float], rotation: Rotation) -> None:
         self.translation = np.array(translation)
         self.rotation = rotation
 
-        partial_translation = np.vstack(self.rotation.rotation_matrix, self.translation)
-        self.translation_matrix = np.hstack((partial_translation, np.array([0, 0, 0, 1])))
+        partial_transform = np.vstack(self.rotation.rotation_matrix, self.translation)
+        self.transformation_matrix = np.hstack((partial_transform, np.array([0, 0, 0, 1])))
         
-        
+        def __mul__(self, other):
+            new_transformation = self.transformation_matrix @ other.transformation_matrix
+            new_translation = new_transformation[:3, 3]
+            new_rotation = Rotation(new_transformation[:3, :3])
+            return Transformation(new_translation, new_rotation)
