d321f0f3b17983b334ea9cf43f9299a8797c3b29
alexjf <alexjfuhr@gmail.com>
2025-02-12 21:23:25 -0500

    Completed problem 4

--- hw4.py
+++ hw4.py
@@ -1,5 +1,6 @@
 from differential_drive import DifferentialDrive
 from bicycle import Bicycle
+from planar_arm import PlanarArm
 import math
 
 TURTLE_BOT_WHEEL_DIAMETER = .07 # meters
@@ -11,6 +12,9 @@ BICYCLE_WHEEL_DIAMETER =.7 # meters
 BICYCLE_WHEEL_RADIUS = BICYCLE_WHEEL_DIAMETER / 2
 BICYCLE_WHEELBASE = 0.983 # meters
 BICYCLE = Bicycle(BICYCLE_WHEEL_RADIUS, BICYCLE_WHEELBASE)
+
+PLANAR_ARM_LENGTHS = (2., 1.5)
+PLANAR_ARM = PlanarArm(PLANAR_ARM_LENGTHS)
 
 if __name__ == "__main__":
     # Problem 1
@@ -50,6 +54,22 @@ if __name__ == "__main__":
     print()
 
     # Problem 4
+    desired_points = [ # the arm is elevated by .5 so I'm subtracting .5 from the y's
+        [2., 1.5],
+        [2.82, 2.82],
+        [2.47, 2.47],
+        [.5, .5]
+    ]
+    for point in desired_points:
+        x, y = point
+        adjusted_y = y -.5
+
+        if PLANAR_ARM.is_reachable((x, adjusted_y)):
+            angles_1, angles_2 = PLANAR_ARM.calculate_segment_angles((x, adjusted_y))
+            print(f"Point {point} can be reached with {angles_1} or {angles_2}")
+        else:
+            print(f"Point {point} is unreachable by the arm")
+    print()
 
     # Problem 5
     forward_velocity = .25 # m/s
--- planar_arm.py
+++ planar_arm.py
@@ -1,24 +1,50 @@
 import math
 
+"""A class for a PlanarArm with any number of segments. Not all methods are fully implemented."""
 class PlanarArm:
-    def __init__(self, arm_lengths: list[float]):
-        self.arm_lengths = arm_lengths
-        self.arms = len(arm_lengths)
+    def __init__(self, segment_lengths: list[float]):
+        self.segment_lengths = segment_lengths
+        self.total_segments = len(segment_lengths)
 
     def calculate_end_effector_position(self, joint_angles: list[float]):
         x, y, angle_sum = 0
-        for i in range(self.arms):
+        for i in range(self.total_segments):
             angle_sum += joint_angles[i]
-            x += math.cos(angle_sum) * self.arm_lengths[i]
-            y += math.sin(angle_sum) * self.arm_lengths[i]
+            x += math.cos(angle_sum) * self.segment_lengths[i]
+            y += math.sin(angle_sum) * self.segment_lengths[i]
         return x, y
     
-    def calculate_arm_angles(self, end_effector_position: tuple[float, float]):
-        if self.arms == 1:
+    def calculate_segment_angles(self, end_effector_position: tuple[float, float]):
+        x, y = end_effector_position
+        if self.total_segments == 1:
             raise NotImplementedError
-        elif self.arms == 2:
-            raise NotImplementedError
-        elif self.arms == 3:
-            raise NotImplementedError
+        elif self.total_segments == 2:
+            if (not self.is_reachable(end_effector_position)):
+                raise ValueError
+            length1 = self.segment_lengths[0]
+            length2 = self.segment_lengths[1]
+
+            angle_2 = math.acos(
+                (x ** 2 + y ** 2 - length1 ** 2 - length2 ** 2) /
+                (2 * length1 * length2)
+            )
+
+            angle_1a = math.atan2(y, x) - math.atan2(length2 * math.sin(angle_2), length1 + length2 * math.cos(angle_2))
+            angle_1b = math.atan2(y, x) - math.atan2(length2 * math.sin(-angle_2), length1 + length2 * math.cos(-angle_2))
+
+            return (angle_1a, angle_2), (angle_1b, -angle_2)
         else:
             raise NotImplementedError
+
+    def is_reachable(self, position: tuple[float, float]):
+        x, y = position
+        radial_distance = math.sqrt(x ** 2 + y ** 2)
+        if self.total_segments == 1:
+            raise NotImplementedError
+        elif self.total_segments == 2:
+            length_1 = self.segment_lengths[0]
+            length_2 = self.segment_lengths[1]
+            r_min = abs(length_1 - length_2)
+            r_max = length_1 + length_2
+
+            return r_min <= radial_distance <= r_max
